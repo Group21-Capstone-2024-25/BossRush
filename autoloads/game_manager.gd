92624545eb00f66257de5f2485ab807e4c6d2177
Alan Saji <rockstaralansaji@gmail.com>
2025-04-15 22:27:34 +0530

    Attached new game btn to a scn

--- autoloads/game_manager.gd
+++ autoloads/game_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const main_menu: = preload("res://content/screens/main_menu/main_menu.tscn")
+const new_game_scene := preload("res://prototype/scenes/new_game_scene.tscn")
 
 var init : bool = false
 
@@ -9,4 +10,8 @@ func start_intro() -> void:
 	
 func open_main_menu() -> void:
 	print("openening main menu")
+
+func start_game():
+	print("game started")
 	
+	get_tree().change_scene_to_packed(new_game_scene)
--- content/screens/main_menu/buttons.gd
+++ content/screens/main_menu/buttons.gd
@@ -2,19 +2,25 @@ extends Control
 class_name MenuButtons
 @onready var settings_button: Button = $"HBoxContainer/VBoxContainer/NinePatchRect3/settings-button"
 @onready var quit_button: Button = $"HBoxContainer/VBoxContainer/NinePatchRect4/quit-button"
+@onready var new_game_button: Button = $"HBoxContainer/VBoxContainer/NinePatchRect/new-game-button"
 
 
 signal settings_pressed
+signal new_game_pressed
 func _ready() -> void:
 	handle_connect_signals()
 	
 func handle_connect_signals() ->void:
 	settings_button.button_down.connect(_on_settings_clicked)
 	quit_button.pressed.connect(_on_quit_pressed)
+	new_game_button.pressed.connect(_on_new_game_clicked)
 	
 func _on_settings_clicked() -> void:
 	print("settings clicked")
-	settings_pressed.emit()  # 🔥 Emit signal to parent
+	settings_pressed.emit()  #  Emit signal to parent
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+func _on_new_game_clicked() -> void:
+	new_game_pressed.emit()
+	
