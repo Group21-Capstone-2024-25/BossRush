74915fe72fac8cb4a95b9a531ddfe9aa7060be76
Rishikesh M <rishikesh7016@gmail.com>
2025-04-18 18:07:00 +0530

    UI changes

---
+++ content/menus/pause_menu/pause_menu.gd
@@ -0,0 +1,29 @@
+extends Control
+
+
+func _on_resume_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_save_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_load_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_select_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_back_button_pressed() -> void:
+	pass # Replace with function body.
