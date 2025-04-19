2089d53586b0b63b7c0fa86d1eac709fc1865683
Rishikesh M <rishikesh7016@gmail.com>
2025-04-20 04:03:42 +0530

    Colosseum update

---
+++ content/actors/asterion/asterion.gd
@@ -0,0 +1,4 @@
+extends Actor
+
+
+@onready var player = get_tree().get_first_node_in_group("Player")
--- resources/ai/tasks/chase.gd
+++ resources/ai/tasks/chase.gd
@@ -3,15 +3,13 @@ extends BTAction
 func _tick(delta: float) -> Status:
 	if agent.player == null:
 		return FAILURE
-
+	
 	var player_pos = agent.player.global_transform.origin
 	var distance = agent.global_transform.origin.distance_to(player_pos)
-
-	# Move toward player
+	
 	agent.chase_player(delta)
-
-	# If close enough to attack, stop chasing
+	
 	if distance <= agent.ATTACK_RANGE:
-		return SUCCESS  # Let the BT move to attack node
-
+		return SUCCESS
+	
 	return RUNNING
