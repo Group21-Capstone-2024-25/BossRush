b0512788a42e72e70033d6e023e73f5174835545
SYNX7007 <ankit.sankar2021@vitbhopal.ac.in>
2025-04-20 17:03:46 +0400

    Behaviour tree Reupload

--- resources/ai/tasks/light_attack.gd
+++ resources/ai/tasks/light_attack.gd
@@ -5,7 +5,7 @@ const ATTACK_DURATION := 1.0
 
 func _enter():
 	timer = 0.0
-	agent.play_animation("minotaur_attack_360_low")
+	agent.play_animation("minotaur_melee_combo_attack_1")
 
 func _tick(delta: float) -> Status:
 	timer += delta
