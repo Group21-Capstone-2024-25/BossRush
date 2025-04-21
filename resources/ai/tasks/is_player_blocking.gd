05642f21efdde44f14ddd0c1602d21fb3f98fbaf
Rishikesh M <rishikesh7016@gmail.com>
2025-04-21 21:42:06 +0530

    Revert "new tasks"
    This reverts commit 9c231dc77268088d7ccf3a9714ac723a45f3c5f6.

--- resources/ai/tasks/is_player_blocking.gd
+++
@@ -1,7 +0,0 @@
-extends BTAction
-
-func _tick(delta: float) -> Status:
-	if agent.check_if_player_is_blocking():
-		return SUCCESS
-	else:
-		return FAILURE
--- resources/ai/tasks/is_player_evading.gd
+++
@@ -1,7 +0,0 @@
-extends BTCondition
-
-func _tick(_delta: float) -> Status:
-	#print("attack working")
-	if agent.player.is_evading:
-		return SUCCESS
-	return FAILURE
